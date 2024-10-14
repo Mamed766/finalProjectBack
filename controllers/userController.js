f54a4cf9ef14f5b40a5a59421f626f8e50a75fb9
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-14 17:33:36 +0400

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 // userController.js
-
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
 
 const updateUserProfile = async (req, res) => {
   const userId = req.params.id;
-  const { firstName, lastName, username, password } = req.body;
+  const { firstName, lastName, username, password, email } = req.body;
 
   try {
     const user = await User.findById(userId);
@@ -21,10 +21,22 @@ const updateUserProfile = async (req, res) => {
     user.firstName = firstName || user.firstName;
     user.lastName = lastName || user.lastName;
     user.username = username || user.username;
-
+    user.email = email || user.email;
     await user.save();
 
-    res.status(200).json({ message: "Profile updated successfully" });
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWTPRIVATEKEY,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Profile updated successfully", token });
   } catch (error) {
     res.status(500).send("Error updating profile: " + error.message);
   }
